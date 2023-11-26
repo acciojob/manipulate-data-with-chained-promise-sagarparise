b3bc9e4a8abdacddf86ebd570d80d2fb1baaf144
sagarparise <141607123+sagarparise@users.noreply.github.com>
2023-11-26 21:58:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
 //your JS code here. If required.
 function manipulateData(inputArray) {
-  // Step 1: Create a promise that resolves after 3 seconds
-  const initialPromise = new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(inputArray);
-    }, 3000);
-  });
-
-  // Step 2: Chain the promises to filter odd numbers after 1 second
-  const oddFilterPromise = initialPromise.then((result) => {
-    return new Promise((resolve) => {
+  return new Promise(async (resolve) => {
+    // Step 1: Resolve the initial promise after 3 seconds
+    const resultAfter3Seconds = await new Promise((innerResolve) => {
       setTimeout(() => {
-        const filteredArray = result.filter((num) => num % 2 === 0);
-        resolve(filteredArray);
+        innerResolve(inputArray);
+      }, 3000);
+    });
+
+    // Step 2: Filter odd numbers after 1 second
+    const resultAfter1Second = await new Promise((innerResolve) => {
+      setTimeout(() => {
+        const filteredArray = resultAfter3Seconds.filter((num) => num % 2 === 0);
+        innerResolve(filteredArray);
       }, 1000);
     });
-  });
 
-  // Step 3: Chain the promises to multiply even numbers by 2 after 2 seconds
-  const multiplyByTwoPromise = oddFilterPromise.then((result) => {
-    return new Promise((resolve) => {
+    // Step 3: Multiply even numbers by 2 after 2 seconds
+    const finalResult = await new Promise((innerResolve) => {
       setTimeout(() => {
-        const multipliedArray = result.map((num) => (num % 2 === 0 ? num * 2 : num));
-        resolve(multipliedArray);
+        const multipliedArray = resultAfter1Second.map((num) => (num % 2 === 0 ? num * 2 : num));
+        innerResolve(multipliedArray);
       }, 2000);
     });
-  });
 
-  // Step 4: Update the text of the HTML element with the final result
-  multiplyByTwoPromise.then((result) => {
+    // Step 4: Update the text of the HTML element with the final result
     const outputElement = document.getElementById('output');
-    outputElement.textContent = result.toString();
-  });
+    outputElement.textContent = finalResult.toString();
 
-  // Return the initial promise to allow further chaining if needed
-  return initialPromise;
+    // Resolve the main promise with the final result
+    resolve(finalResult);
+  });
 }
 
 // Example usage with the input array [1, 2, 3, 4]
